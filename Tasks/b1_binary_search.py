24de52d576b6e8ddd07433175a89afbcf342afda
Anatoliy Levchuk <feuerlag999@gmail.com>
2020-05-30 12:55:28 +0300

    init

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -1,5 +1,7 @@
 from typing import Sequence, Optional
 
+arr = range(0, 100)
+
 
 def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     """
@@ -9,5 +11,24 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+    l = 0
+    r = len(arr) - 1
+
+    while r - l > 2:
+        m = (l + r) // 2
+        if elem > arr[m]:
+            l = m + m % 2
+        else:
+            r = m - m % 2 + 1
+
+    if elem == arr[l]:
+        return l
+    if elem == arr[r]:
+        return r
+    else:
+        return None
+
+
+print(10//2)
+print(11//2)
+print(9//2)
