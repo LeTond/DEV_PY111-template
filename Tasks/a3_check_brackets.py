57b4c6cd14af8ecf480feee73dd13cb461140768
Anatoliy Levchuk <feuerlag999@gmail.com>
2020-06-04 22:17:43 +0300

    init

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,5 +5,29 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
+    count_open = 0
+    count_close = 0
+    if len(brackets_row) > 0:
+        for br in range(0, len(brackets_row)):
+            if brackets_row[br] == "(":
+                count_open += 1
+            elif brackets_row[br] == ")":
+                count_close += 1
 
-    return False
+    if count_open == count_close:
+        for br2 in range(len(brackets_row)):
+            if brackets_row[0] == ")" or brackets_row[-1] == "(":
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    print(check_brackets("()()"))
+    print(check_brackets(""))
+    print(check_brackets("()()(()())"))
+    print(check_brackets(")("))
+    print(check_brackets(")"))
+    print(check_brackets("("))
