bf6a10d32ceb08098214a8127fb881b0711e1f6c
Anatoliy Levchuk <feuerlag999@gmail.com>
2020-06-06 19:38:25 +0300

    Рабочий вариант 2

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -19,7 +19,6 @@ def ex(x: Union[int, float]) -> float:
         result += step
         n += 1
         step = x ** n / fact(n)
-
     return result
 
 
@@ -31,13 +30,13 @@ def sinx(x: Union[int, float]) -> float:
     :return: sin(x) value
     """
     sinx_x = x
-    n = 0
-    step = (-1) ** n * x ** (2 * n + 1) / fact_sin(n)
+    n = 1
+    step = (-1) ** n * x ** (2 * n + 1) / fact(2 * n + 1)
 
     while abs(step) > 0.0001:
         sinx_x += step
         n += 1
-
+        step = (-1) ** n * x ** (2 * n + 1) / fact(2 * n + 1)
     return sinx_x
 
 
@@ -49,9 +48,5 @@ def fact(n):
     return factorial
 
 
-def fact_sin(n):
-    factorial = 3
-    while n > 1:
-        factorial *= (2 * n + 1)
-        n -= 1
-    return factorial
+if __name__ == "__main__":
+    print(sinx(3))
