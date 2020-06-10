6234ca0e7e58f58be7526a550b1e45096b189ae8
Anatoliy Levchuk <feuerlag999@gmail.com>
2020-06-10 12:42:46 +0300

    Рабочая версия

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,5 +1,8 @@
 from typing import Hashable, List
+import matplotlib.pyplot as plt
 import networkx as nx
+
+list_ = []
 
 
 def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -10,5 +13,21 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node for search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    g2 = nx.Graph()
+    g2.add_node(start_node)
+    list_ = list(g.neighbors(start_node))
+
+    while len(list(g2.nodes)) < len(list(g.nodes)):
+        for i in range(len(list_) - 1):
+            if list_[i] == list(g2.nodes)[0]:
+                list_.remove(list_[i])
+        else:
+            g2.add_node(list_[0])
+            list_ += list(g.neighbors(list_[0]))
+            list_.remove(list_[0])
+    # nx.draw(g, with_labels=True)
+    # plt.show()
+    return list(g2.nodes)
+
+# if __name__ == '__main__':
+# bfs(graph, start_node="A")
