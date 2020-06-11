10e63d883e3636851c4aa3d26cf3090d46610917
Anatoliy Levchuk <feuerlag999@gmail.com>
2020-06-11 14:03:49 +0300

    Рабоча версия

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -1,5 +1,9 @@
 from typing import Hashable, List
 import networkx as nx
+import matplotlib.pyplot as plt
+from Tasks.a0_my_stack import *
+
+list_ = []
 
 
 def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -10,5 +14,20 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    g2 = nx.Graph()
+    list_ = list(g.neighbors(start_node))
+    g2.add_node(start_node)
+
+    while len(list(g2.nodes)) < len(list(g.nodes)):
+        if list_[0] in list(g2.nodes):
+            list_.remove(list_[0])
+        else:
+            g2.add_node(list_[0])
+            list_2 = list(g.neighbors(list_[0]))
+            list_.remove(list_[0])
+            for i in range(len(list_2) - 1):
+                if list_2[0] in list(g2.nodes):
+                    list_2.remove(list_2[0])
+            list_ = list_2 + list_
+
+    return list(g2.nodes)
