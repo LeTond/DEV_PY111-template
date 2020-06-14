579d2c4e84ee49ccdf9476c5a6ce34277df3e87b
Anatoliy Levchuk <feuerlag999@gmail.com>
2020-06-14 19:14:11 +0300

    Рабочая версия

--- Tasks/e2_dijkstra.py
+++ Tasks/e2_dijkstra.py
@@ -2,8 +2,8 @@ from typing import Hashable, Mapping, Union
 import networkx as nx
 import matplotlib.pyplot as plt
 
-diction = {}
 list_ = []
+
 
 def dijkstra_algo(g: nx.DiGraph, starting_node: Hashable) -> Mapping[Hashable, Union[int, float]]:
     """
@@ -12,23 +12,24 @@ def dijkstra_algo(g: nx.DiGraph, starting_node: Hashable) -> Mapping[Hashable, U
     :param starting_node: starting node from g
     :return: dict like {'node1': 0, 'node2': 10, '3': 33, ...} with path costs, where nodes are nodes from g
     """
-    print(g, starting_node)
+    diction = {}
 
     for i in g.nodes:
         diction[i] = float("inf")
 
-    list_ = list(g.neighbors(starting_node))
-    diction[list_[0]] = len(list(g.neighbors(starting_node)))
+    diction[starting_node] = 0
+    len_g = len(list(g.edges))
+    exit_ = True
 
-    while len(list_) < len(list(g.nodes)):
-        for i in range(len(list(g.nodes)) - 1):
-            for j in range(len(list(g.neighbors(list(g.nodes)[i]))) - 1):
-                if list(g.neighbors(list(g.nodes)[i]))[j] != list_[-1]:
-                    diction[list(g.nodes)[i]] += g[list(g.nodes)[i]][list(g.neighbors(list(g.nodes)[i]))[j]]["weight"]
-                    list_ += list(g.neighbors(list(g.nodes)[i]))[j]
+    while exit_:
+        exit_ = False
+        for i in range(len_g):
+            k = list(g.edges)[i][0]
+            h = list(g.edges)[i][1]
+            if g[k][h]["weight"] + diction[k] < diction[h]:
+                diction[h] = g[k][h]["weight"] + diction[k]
+                exit_ = True
 
-    print(g["B"]["E"]["weight"])
-    print(list_)
     print(diction)
     # nx.draw(g, with_labels=True)
     # plt.show()
