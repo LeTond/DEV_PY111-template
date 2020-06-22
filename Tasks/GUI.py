a72c34deab43499614e25b770a9bdc3fddaed82d
Anatoliy Levchuk <feuerlag999@gmail.com>
2020-06-22 19:45:03 +0300

    Реализовано удаление и добавление книг в библиотеку через argparse

--- Tasks/GUI.py
+++ Tasks/GUI.py
@@ -1,29 +1,26 @@
-
 from Tasks.enter_task import *
 
 from tkinter import *
 from tkinter import scrolledtext
 
+
 def gui_interface(output):
     window = Tk()
-
     window.title("Библиотека художественной литературы")
     window.geometry('2000x1400+200+300')
 
     txt = scrolledtext.ScrolledText(window, width=120, height=30, padx=10, pady=5)
-    txt.place(x=15, y=400)
     txt.insert(END, output + '\n')
 
     text = Entry(window, width=100)
-    text.place(x=15, y=50)
+    global key
+    key = text.get()
 
     btn = Button(window, text="Кнопка ввода!", padx=10, pady=5, command=text)
+
+    txt.place(x=15, y=400)
+    text.place(x=15, y=50)
     btn.place(x=1500, y=100)
 
     window.mainloop()
-
-
-
-
-
 
--- Tasks/enter_task.py
+++ Tasks/enter_task.py
@@ -1,89 +1,115 @@
 """
-Данное задание необходимо выполнить до конца курса. Все неоднозначные или не упомянутые моменты могут
-трактоваться вами в любую сторону по вашему желанию. Решение должно представлять собой модуль с готовой реализацией,
-прикрепленный в качестве ответа. По любым вопросам можно обращаться к преподавателю или задавать в форуме
-"Вопросы и ответы", но запрещено делиться реализацией с другими людьми.
-
-Использовать готовые решение запрещается, при сдаче работы будьте готовы к тому, что у преподавателя будут вопросы по
-нюансам реализации или работе той или иной подсистемы. Сдача задания после указанного срока может привести к появлению
-дополнительных заданий для вас.
-
-Сдача задания является одним из обязательных условий получения зачета по курсу PY111.
-
-Задание:
-
 Дан каталог книг. Реализуйте библиотеку для хранения данных книг и поиску по каталогу. Каталог должен поддерживать
 возможность добавления и удаления книг, редактирования информации о книге, а также обладать персистентностью
 (т.е. сохранять библиотеку в внешнем файле и подгружать обратно). Также необходимо оформить точку входа,
 поддерживать поиск по различным параметрам и обеспечить интерфейс взаимодействия пользователя с библиотекой.
+
+Пример запуска кода:
+python3 Tasks/enter_task.py show
+python3 Tasks/enter_task.py -key Дойл show
+python3 Tasks/enter_task.py -bk 8 -st Фантастика -au Ефремов -wo 'Туманность Андромеды' -ye 1957 save
+python3 Tasks/enter_task.py -bk 8 delete
 """
-from Tasks.GUI import *
+import argparse
 import json
 
-# Реализация каталога через словарь словарей
-book_list = {
-    1: {'style': 'Фантастика', 'author': "Желязны", 'work': "Девять принцев Амбера", 'year': "1970"},
-    2: {'style': 'Фантастика', 'author': "Желязны", 'work': "Ружья Авалона", 'year': "1972"},
-    3: {'style': 'Фантастика', 'author': "Желязны", 'work': "Знак Единорога", 'year': "1975"},
-    4: {'style': 'Фантастика', 'author': "Желязны", 'work': "Рука Оберона", 'year': "1976"},
-    5: {'style': 'Детектив', 'author': "Дойл", 'work': "Скандал в Богемии", 'year': "1982"},
-    6: {'style': 'Детектив', 'author': "Дойл", 'work': "Союз рыжих", 'year': "1982"}
-}
-
-book_add = {
-    7: {'style': 'Детектив', 'author': "Дойл", 'work': "Собака Баскервилей", 'year': "1901"}
-}
-
-json_file = 'library'
+json_file = 'Tasks/library'
+list_book = ""
 
 
-def library_from_json(json_file):
-    # with open(json_file, "w", encoding="utf-8") as file:
-    #     json.dump(book_list, file)
-    #     print(json.dumps(book_list, indent=4, ensure_ascii=False))
-    with open(json_file, 'r') as file:
-        template = json.load(file)
+def search_in_library(json_file: str, list_book: str) -> None:
+    parser = create_parser()
+    namespace = parser.parse_args()
+    search_key_name = namespace.key_word
 
-        with open(json_file, "w", encoding="utf-8") as file:
-            # Добавляем новую книгу с новым ключем
-            template['7'] = {'style': 'Детектив', 'author': "Не Дойл", 'work': "Собака Баскервилей", 'year': "1901"}
-            # Удаляем книгу по ключю
-            # template.pop('7', None)
-            json.dump(template, file)
-            print(json.dumps(template, indent=4, ensure_ascii=False))
-            # print(template)
-            # print(template.keys())
-
-
-def search_in_library(json_file):
-
-    list_book = ""
-    # gui_interface(list_book)
-    search_key_name = input("Введите ключевое слово: ")
-    # search_key_name = text
     flag = True
-
-    with open(json_file, 'r') as file:
-        template = json.load(file)
-
-        for search_book_list in template:
-            if template[search_book_list]['style'] == search_key_name \
-                    or template[search_book_list]['work'] == search_key_name \
-                    or template[search_book_list]['author'] == search_key_name \
-                    or template[search_book_list]['year'] == search_key_name:
+    if namespace.command == 'show':
+        with open(json_file, 'r') as file:
+            template = json.load(file)
+            for search_book_list in template:
                 print_ = f"№: {search_book_list} / " \
                          f"Жанр: {template[search_book_list]['style']} / " \
                          f"Автор: {template[search_book_list]['author']} / " \
                          f"Произведение: {template[search_book_list]['work']} / " \
                          f"Год издания: {template[search_book_list]['year']}"
-                list_book += print_ + '\n'
-                flag = False
-        if flag:
-            print_ = 'Данная книга отсутствует в библиотеке'
-            gui_interface(print_)
-    gui_interface(list_book)
+                if template[search_book_list]['style'] == search_key_name \
+                        or template[search_book_list]['work'] == search_key_name \
+                        or template[search_book_list]['author'] == search_key_name \
+                        or template[search_book_list]['year'] == search_key_name:
+                    list_book += print_ + '\n'
+                    flag = False
+                if search_key_name == None:
+                    print(print_)
+                    flag = False
+            if flag:
+                print_ = 'Данная книга отсутствует в библиотеке'
+                print(print_)
+        print(list_book)
+    elif namespace.command == 'save':
+        with open(json_file, 'r') as file:
+            template = json.load(file)
+            with open(json_file, "w", encoding="utf-8") as file:
+                template[namespace.book] = {'style': namespace.style,
+                                            'author': namespace.author,
+                                            'work': namespace.work,
+                                            'year': namespace.year}
+                json.dump(template, file)
+
+    elif namespace.command == 'delete':
+        with open(json_file, 'r') as file:
+            template = json.load(file)
+            with open(json_file, "w", encoding="utf-8") as file:
+                template.pop(namespace.book, None)
+                json.dump(template, file)
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-key",
+                        "--key_word",
+                        type=str,
+                        help="Ключевое слово для поиска произведения")
+
+    parser.add_argument("-bk", "--book",
+                        type=str,
+                        help="Номер нового произведения")
+
+    parser.add_argument("-st", "--style",
+                        type=str,
+                        help="Жанр нового произведения")
+
+    parser.add_argument("-au", "--author",
+                        type=str,
+                        help="Автор нового произведения")
+
+    parser.add_argument("-wo", "--work",
+                        type=str,
+                        help="Название нового произведения")
+
+    parser.add_argument("-ye", "--year",
+                        type=str,
+                        help="Год нового произведения")
+
+    subparsers = parser.add_subparsers(dest="command")
+
+    create_show_subparser(subparsers)
+    create_save_subparser(subparsers)
+    create_delete_subparser(subparsers)
+
+    return parser
+
+
+def create_show_subparser(subparsers):
+    show_parser = subparsers.add_parser('show', help="Режим вывода в консоль")
+
+
+def create_save_subparser(subparsers):
+    save_parser = subparsers.add_parser('save', help="Режим добавления в файл")
+
+
+def create_delete_subparser(subparsers):
+    delete_parser = subparsers.add_parser('delete', help="Режим удаления книги из библиотеки")
 
 if __name__ == '__main__':
-    library_from_json(json_file)
-    search_in_library(json_file)
+    search_in_library(json_file, list_book)
+
 
